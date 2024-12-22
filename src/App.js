ce65d90d0ffd9049758e3778cba2f69d906c4c8d
zam <zam@linuxbox.local>
2024-12-22 17:25:21 -0600

    basic react ui template

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to My Website</h1>
+      <p>This is a basic React website.</p>
+    </div>
+  );
+}
+
+export default App;
